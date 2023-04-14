f76cf49d4f9eea9202fa93f90577963003ed73d8
João Rangel <joao.vran@gmail.com>
2023-04-14 00:10:44 -0300

    feat: implement User and UserRepository

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,30 @@
 import { v4 as uuidV4 } from "uuid";
 
+interface IUserPayload {
+  id?: string;
+  name: string;
+  email: string;
+  admin?: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
 class User {
-  // Complete aqui
+  constructor(payload?: IUserPayload) {
+    Object.assign(this, payload);
+  }
+
+  id = uuidV4();
+
+  name: string;
+
+  email: string;
+
+  admin = false;
+
+  created_at = new Date();
+
+  updated_at = new Date();
 }
 
 export { User };
